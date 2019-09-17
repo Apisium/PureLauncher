e18da0efb9a3cbdf5df3c39a2a7470b286952e4e
ci010 <cijhn@hotmail.com>
2019-09-17 11:39:57 +0800

    POC of the profile save/load

--- src/models/ProfilesModel.ts
+++ src/models/ProfilesModel.ts
@@ -1,5 +1,50 @@
 import { Model } from 'use-model'
-// import fse from 'fs-extra'
+import fs from 'fs-extra';
+
+/// minecraft root utils start ///
+
+import { remote } from 'electron';
+import { join } from 'path';
+import { platform } from 'os';
+
+function getMinecraftRoot() {
+  return join(remote.app.getPath("appData"), platform() === 'darwin' ? 'minecraft' : '.minecraft');
+}
+
+/// minecraft root utils end ///
+
+/// java utils start ///
+
+import { exec } from 'child_process';
+
+function getJavaVersion(path: string) {
+  const parseVersion = (str: string) => {
+    const match = /(\d+\.\d+\.\d+)(_(\d+))?/.exec(str);
+    if (match === null) return undefined;
+    return match[1];
+  };
+  return new Promise<string>((resolve, reject) => {
+    exec(`${path} -version`, (err, stdout, serr) => {
+      if (!serr) {
+        resolve(undefined);
+      } else {
+        const version = parseVersion(serr);
+        if (version !== undefined) {
+          resolve(version);
+        } else {
+          resolve(undefined);
+        }
+      }
+    });
+  });
+}
+
+/// java utils end ///
+
+
+const LAUNCH_PROFILE = 'launch_profile.json';
+const EXTRA_CONFIG = 'any_profile.json';
+
 
 interface Version {
   name: string
@@ -26,8 +71,8 @@ export default class ProfilesModel extends Model {
     account: '',
     profile: ''
   }
-  public profiles: { [key: string]: Version } = { }
-  public authenticationDatabase: { [key: string]: User } = { }
+  public profiles: { [key: string]: Version } = {}
+  public authenticationDatabase: { [key: string]: User } = {}
   public clientToken = ''
   public extraJson = {
     javaPath: '',
@@ -39,47 +84,97 @@ export default class ProfilesModel extends Model {
       '-XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'
   }
 
-  constructor () {
+  readonly launchProfilePath: string;
+  readonly extraConfigPath: string;
+
+  constructor(readonly root = getMinecraftRoot()) {
     super()
-    // TODO: loadConfigSync
+    this.launchProfilePath = join(this.root, LAUNCH_PROFILE);
+    this.extraConfigPath = join(this.root, EXTRA_CONFIG);
+    try {
+      this.loadLaunchProfileJson(fs.readJsonSync(this.launchProfilePath))
+    } catch (e) {
+      this.onLoadLaunchProfileFailed(e);
+    }
+
+    try {
+      this.loadExtraConfigJson(fs.readJsonSync(this.extraConfigPath))
+    } catch (e) {
+      this.onLoadExtraConfigFailed(e);
+    }
   }
 
-  public * setJavaPath (path: string) {
+  public * setJavaPath(path: string) {
     // TODO: Check java exists
-    yield* this.saveConfig()
+    const version = yield getJavaVersion(path);
+    if (version) {
+      this.extraJson.javaPath = path;
+      yield* this.saveExtraConfigJson();
+    }
   }
 
-  public * loadConfig () {
-    // TODO:fs-extra.readJson()
+  private loadLaunchProfileJson(json: any) {
+    this.selectedUser = json.selectedUser;
+    this.authenticationDatabase = json.authenticationDatabase;
+    this.clientToken = json.clientToken;
+    this.settings = json.settings;
+    this.profiles = json.profiles;
   }
 
-  public * saveConfig () {
-    // TODO:Async!!!!
+  private loadExtraConfigJson(extra: this['extraJson']) {
+    this.extraJson = extra;
   }
 
-  public * setMemory (mem: string) {
+  private onLoadLaunchProfileFailed(e: any) {
+
+  }
+
+  private onLoadExtraConfigFailed(e: any) {
+
+  }
+
+  public * loadAllConfigs() {
+    yield fs.readJson(this.launchProfilePath).then(j => this.loadLaunchProfileJson(j), e => this.onLoadLaunchProfileFailed(e))
+    yield fs.readJson(this.extraConfigPath).then(j => this.loadExtraConfigJson(j), e => this.onLoadExtraConfigFailed(e))
+  }
+
+  public * saveLaunchProfileJson() {
+    yield fs.writeJson(this.launchProfilePath, {
+      settings: this.selectedUser,
+      selectedUser: this.selectedUser,
+      profile: this.profiles,
+      authenticationDatabase: this.authenticationDatabase,
+      clientToken: this.clientToken,
+    })
+  }
+
+  public * saveExtraConfigJson() {
+    yield fs.writeJson(this.extraConfigPath, this.extraJson)
+  }
+
+  public * setMemory(mem: string) {
     const m = parseInt(mem, 10)
     this.extraJson.memory = Number.isNaN(m) || Object.is(m, Infinity) || m < 0 ? 0 : m
-    yield* this.saveConfig()
+    yield* this.saveExtraConfigJson()
   }
 
-  public * toggleBmclAPI () {
+  public * toggleBmclAPI() {
     this.extraJson.bmclAPI = !this.extraJson.bmclAPI
-    yield* this.saveConfig()
+    yield* this.saveExtraConfigJson()
   }
 
-  public * setArgs (args: string) {
+  public * setArgs(args: string) {
     this.extraJson.javaArgs = args
-    yield* this.saveConfig()
+    yield* this.saveExtraConfigJson()
   }
 
-  public * toggleSound () {
+  public * toggleSound() {
     this.settings.soundOn = !this.settings.soundOn
-    yield* this.saveConfig()
+    yield* this.saveLaunchProfileJson()
   }
 
-  public * toggleShowLog () {
+  public * toggleShowLog() {
     this.settings.showGameLog = !this.settings.showGameLog
-    yield* this.saveConfig()
+    yield* this.saveLaunchProfileJson()
   }
 }
