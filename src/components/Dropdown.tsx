8413452b0ee3e910a3fc548d4eb40813f81d87dc
Shirasawa <764798966@qq.com>
2019-10-10 18:08:39 +0800

    i18n: LoginDialog

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,19 +1,22 @@
 import './dropdown.less'
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 
 const poses = {
   open: { width: 'auto' },
   closed: { width: 0 }
 }
-const Dropdown: React.FC<{ open: boolean }> = (props) =>
-  <motion.div
+const Dropdown: React.FC<{ open: boolean }> = (props) => {
+  const [hover, setHover] = useState(false)
+  return <motion.div
     initial={poses.closed}
-    whileHover={poses.open}
-    animate={props.open ? 'open' : 'closed'}
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+    animate={props.open || hover ? poses.open : poses.closed}
     className='dropdown'
   >
     <div className='cover'>{props.children}</div>
   </motion.div>
+}
 
 export default Dropdown
--- src/components/LoginDialog.tsx
+++ src/components/LoginDialog.tsx
@@ -1,7 +1,9 @@
 import './login-dialog.less'
-import React, { useState, InvalidEvent } from 'react'
+import React, { useState } from 'react'
 import Dialog from 'rc-dialog'
+import { shell } from 'electron'
 
+const buy = () => shell.openExternal('https://my.minecraft.net/store/minecraft/#register')
 const steve = { backgroundImage: `url(${require('../assets/images/steve.png')})` }
 const zombie = { backgroundImage: `url(${require('../assets/images/zombie.png')})` }
 
@@ -12,10 +14,12 @@ const LoginDialog: React.FC = () => {
   return <Dialog className='login-dialog'>
     {type === 0
       ? <>
-          <p className='title'>选择您的帐号登录模式, 如果您还没有购买正版, 请您选择右侧的“离线登录”</p>
+          <p className='title'>{$('Choose your account login mode. If you don\'t have the online version, please choose the "Offline Login" on the right')}</p>
           <div className='heads'>
-            <div><div className='head' style={steve} data-sound onClick={() => setType(1)} /><p>正版登录</p></div>
-            <div><div className='head' style={zombie} data-sound onClick={() => setType(2)} /><p>离线登录</p></div>
+            <div><div className='head' style={steve} data-sound onClick={() => setType(1)} />
+              <p>{$('Online Login')}</p></div>
+            <div><div className='head' style={zombie} data-sound onClick={() => setType(2)} />
+              <p>{$('Offline Login')}</p></div>
           </div>
         </>
       : type === 1
@@ -23,7 +27,7 @@ const LoginDialog: React.FC = () => {
           setSubmitted(true)
           e.preventDefault()
         }} onInvalid={() => setSubmitted(true)}>
-            <label>邮箱</label>
+            <label>{$('Email')}</label>
             <div className='input2'>
               <input required type='email' name='email' />
               <div className='dot0' />
@@ -31,7 +35,7 @@ const LoginDialog: React.FC = () => {
               <div className='dot2' />
               <div className='dot3' />
             </div>
-            <label>密码</label>
+            <label>{$('Password')}</label>
             <div className='input2'>
               <input required type='password' name='password' />
               <div className='dot0' />
@@ -40,17 +44,17 @@ const LoginDialog: React.FC = () => {
               <div className='dot3' />
             </div>
             <div className='links'>
-              <span data-sound className='left' onClick={back}>返回</span>
-              <span data-sound className='right'>购买正版</span>
+              <span data-sound className='left' onClick={back}>{$('Back')}</span>
+              <span data-sound className='right' onClick={buy}>{$('Register')}</span>
             </div>
-            <button type='submit' className='btn3' style={{ marginTop: 8, width: '100%' }}>登录</button>
+            <button type='submit' className='btn3' style={{ marginTop: 8, width: '100%' }}>{$('Login')}</button>
           </form>
         : type === 2
           ? <form className={submitted ? 'submitted' : void 0} onSubmit={e => {
             setSubmitted(true)
             e.preventDefault()
           }} onInvalid={() => setSubmitted(true)}>
-            <label>游戏名</label>
+            <label>{$('Username')}</label>
             <div className='input2'>
               <input required pattern='\w{2,16}' name='username' />
               <div className='dot0' />
@@ -59,9 +63,9 @@ const LoginDialog: React.FC = () => {
               <div className='dot3' />
             </div>
             <div className='links'>
-              <span data-sound className='left' onClick={back}>返回</span>
+              <span data-sound className='left' onClick={back}>{$('Back')}</span>
             </div>
-            <button type='submit' className='btn3' style={{ marginTop: 8, width: '100%' }}>登录</button>
+            <button type='submit' className='btn3' style={{ marginTop: 8, width: '100%' }}>{$('Login')}</button>
           </form>
         : null}
   </Dialog>
