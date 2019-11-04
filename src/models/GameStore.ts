688d3a893eaa974f0cddc9bc59c288b30d247c59
ci010 <cijhn@hotmail.com>
2019-11-05 00:13:15 +0800

    POC for installer

--- src/models/GameStore.ts
+++ src/models/GameStore.ts
@@ -1,6 +1,8 @@
 import { Store, injectStore } from 'reqwq'
 import { Launcher } from '@xmcl/launch'
 import ProfilesStore from './ProfilesStore'
+import { Installer } from '@xmcl/installer'
+import { Version } from '@xmcl/version'
 
 export default class GameStore extends Store {
   public error: { code: number, signal: string } | undefined | any
@@ -34,8 +36,8 @@ export default class GameStore extends Store {
       }
     })
   }
-  public async launch (version?: string) {
-    const { extraJson, root, getCurrentProfile, selectedVersion } = this.profilesStore
+  public * launch (version?: string) {
+    const { extraJson, root, getCurrentProfile, selectedVersion, versionManifest, ensureVersionManifest } = this.profileModel()
     const { javaArgs, javaPath } = extraJson
 
     const { accessToken = '', uuid, username, displayName, type } = getCurrentProfile()
@@ -49,19 +51,27 @@ export default class GameStore extends Store {
       }
     }
 
+    let versionId: string
+
     switch (version) {
       case 'latest-release':
-        // TODO: Get the right version and download
+        yield ensureVersionManifest()
+        versionId = versionManifest.latest.release
+        yield this.ensureMinecraftVersion(root, versionManifest.versions.find(v => v.id === versionId))
         break
       case 'latest-snapshot':
-        // TODO: Get the right version and download
+        yield ensureVersionManifest()
+        versionId = versionManifest.latest.snapshot
+        yield this.ensureMinecraftVersion(root, versionManifest.versions.find(v => v.id === versionId))
         break
       default:
-        // TODO: Check if version exists.
+        versionId = version
+        yield this.ensureLocalVersion(root, versionId)
+        break
     }
 
     const option: Launcher.Option = {
-      version,
+      version: versionId,
       javaPath: javaPath || 'java',
       extraJVMArgs: javaArgs.split(' '),
       auth: {
@@ -95,5 +105,16 @@ export default class GameStore extends Store {
       }
       this.worker.addEventListener('message', onceLaunch)
     })
+  }
+  private async ensureMinecraftVersion (minecraft: string, version: Installer.VersionMeta) {
+    const task = Installer.installTask('client', version, minecraft)
+    // TODO: listen task progress
+    await task.execute()
+  }
+  private async ensureLocalVersion (minecraft: string, versionId: string) {
+    const resolved = await Version.parse(minecraft, versionId)
+    const task = Installer.installDependenciesTask(resolved)
+    // TODO: listen task progress
+    await task.execute()
   }
 }
