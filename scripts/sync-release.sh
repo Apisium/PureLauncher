a155b46946bc690301c2925fb47c1d15a83d123d
Shirasawa <764798966@qq.com>
2020-03-23 11:00:23 +0800

    Fix release

--- scripts/sync-release.sh
+++ scripts/sync-release.sh
@@ -14,14 +14,14 @@ mkdir -p "$FOLDER" || exit -1
 
 for FILE in $FILES; do
 	EXTENSION="${FILE##*.}"
-	[[ "$EXTENSION" != "json" ]] && [[ "$EXTENSION" != "html" ]] || continue
+	[ "$EXTENSION" != "json" ] && [ "$EXTENSION" != "html" ] || continue
 	FROM="$ROOT/$VERSION/$FILE"
 	SAVE_TO="$FOLDER/$VERSION/$FILE"
 	SUPPOSED="`echo "$BODY" | jq -r ".$EXTENSION"`"
 	mkdir -p "`dirname "$SAVE_TO"`"
 	for i in 1 2 3; do
-		[[ -e "$SAVE_TO" ]] && sha1sum "$SAVE_TO" | grep "$SUPPOSED" && break
-		wget -c -t 1 -T 15 -O "$SAVE_TO" "$FROM" || [[ $i -eq 3 ]] && exit -1 || continue
-		sha1sum "$SAVE_TO" | grep "$SUPPOSED" && break  || [[ $i -eq 3 ]] && exit -1 || continue
+		[ -e "$SAVE_TO" ] && sha1sum "$SAVE_TO" | grep "$SUPPOSED" && break
+		wget -c -t 1 -T 15 -O "$SAVE_TO" "$FROM" || [ $i -eq 3 ] && exit -1 || continue
+		sha1sum "$SAVE_TO" | grep "$SUPPOSED" && break  || [ $i -eq 3 ] && exit -1 || continue
 	done
 done
